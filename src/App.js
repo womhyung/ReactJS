4b173d79bec09de315ab64648ece37c5b8e51124
Williams Santos <153226721+womhyung@users.noreply.github.com>
2023-12-07 16:33:49 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from "react";
+
+import api from "./services/api";
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState( [] );
+
+  useEffect( () => {
+    api.get('/repositories').then( response =>
+      setRepositories( response.data )
+    );
+  }, [] );
+
+  async function handleAddRepository() {
+    // TODO
+     const response = await api.post('/repositories', {
+      title: 'Prog Web',
+      url: 'ufopa.edu.br',
+      techs: ['Python', 'Node.JS', 'ReactJS'],
+    })
+
+    setRepositories( [...repositories, response.data ] );
+
+  }
+
+  async function handleRemoveRepository(id) {
+    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(repository => repository.id != id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map( repository => (
+          <li key = {repository.id}>
+          {repository.title}
+
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
